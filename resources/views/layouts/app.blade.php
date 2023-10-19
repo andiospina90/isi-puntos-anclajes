3b289284ffaf82297783b8ef4a435c45e907f060
andiospina90 <andi.os90@gmail.com>
2023-10-18 23:55:53 -0500

    Agregar menu de empresas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,49 +21,86 @@
 
 
     <!-- Styles -->
+    <link href="{{ asset('css/anclajes.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <div class="d-flex justify-content-around" style="width: 100%">
+            <div class="container" style="max-width: 100% !important;">
+                <div style="width: 100%">
                     @if (Auth::check())
-                        <a class="navbar-brand" href="{{ url('/home') }}">
-                            <img alt="Qries"
-                                src="https://isiseguridadindustrial.com/wp-content/uploads/2017/04/png.jpg"style="max-width: 5em;">
-                        </a>
-                        <div class="row" style="display: flex; justify-content: space-around; width: 100%; padding-left: 5%; padding-right: 5%">
-                            <Button class="btn btn-primary col-xs-12 col-md-2 mb-2" style="background-color: orangered; border-color: orangered"><a href="{{ url('/registrarPuntoAnclaje') }}"
-                                    style="text-decoration: none; color: white; font-weight: 500">Registrar
-                                    Precinto</a></Button>
-                            <Button class="btn btn-primary col-xs-12 col-md-2 mb-2" style="background-color: orangered; border-color: orangered"><a href="{{ url('/registrarEmpresa') }}"
-                                    style="text-decoration: none; color: white; font-weight: 500">Registrar
-                                    Empresa</a></Button>
-                            <Button class="btn btn-primary col-xs-12 col-md-2 mb-2" style="background-color: orangered; border-color: orangered"><a href="{{ url('/home') }}"
-                                    style="text-decoration: none; color: white; font-weight: 500">Consultar Sistemas
-                                    de ingeniería</a></Button>
-                                    <Button class="btn btn-primary col-xs-12 col-md-2 mb-2" style="background-color: orangered; border-color: orangered">
-                            <a class="" style="text-decoration: none; color: white; font-weight: 500" href="{{ route('logout') }} "
-                                onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                {{ __('Cerrar Sesion') }}
+                    <div class="row">
+                        <div class="" style="display: flex; justify-content: center;">
+                            <a class="navbar-brand" href="{{ url('/home') }}">
+                                <img alt="Qries"
+                                    src="https://www.isiseguridadindustrial.com/wp-content/uploads/elementor/thumbs/Arte-LOGO-sin-fondo-ptk6xelhcamh2whobcc1e5k0bgdk3l3xwv5q780pgo.png"
+                                    style="max-width: 5em;">
                             </a>
-                            </Button>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered"><a
+                                        href="{{ url('/home') }}"
+                                        style="text-decoration: none; color: white; font-weight: 500">Consultar
+                                        Sistemas
+                                        de ingeniería</a></Button>
+                            </div>
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered"><a
+                                        href="{{ url('/registrarPuntoAnclaje') }}"
+                                        style="text-decoration: none; color: white; font-weight: 500">Registrar
+                                        Precinto</a></Button>
+                            </div>
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered"><a
+                                    href="{{ url('/eliminarPuntosAnclaje') }}"
+                                        style="text-decoration: none; color: white; font-weight: 500">Eliminar serie de precintos</a></Button>
+                            </div>
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered"><a
+                                        href="{{ url('/registrarEmpresa') }}"
+                                        style="text-decoration: none; color: white; font-weight: 500">Registrar
+                                        Empresa</a></Button>
+                            </div>
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered"><a
+                                        href="{{ url('/registrarEmpresa') }}"
+                                        style="text-decoration: none; color: white; font-weight: 500">Consultar Empresas</a></Button>
+                            </div>
+                            <div class="col-lg-2 mb-2" style="display: flex; justify-content: center;">
+                                <Button class="btn btn-primary"
+                                    style="background-color: orangered; border-color: orangered">
+                                    <a class="" style="text-decoration: none; color: white; font-weight: 500"
+                                        href="{{ route('logout') }} " onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        {{ __('Cerrar Sesion') }}
+                                    </a>
+                                </Button>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+
+                        </div>
+                    </div>
+
+
                     @else
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            <img alt="Qries"
-                                src="https://isiseguridadindustrial.com/wp-content/uploads/2017/04/png.jpg"
-                                style="max-width: 5em;">
-                        </a>
-                        <Button class="btn btn-primary" style="background-color: orangered; border-color: orangered"><a
-                                class="nav-link" href="{{ route('login') }}"
-                                style="color:white">{{ __('Iniciar sesión') }}</a></Button>
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img alt="Qries" src="https://isiseguridadindustrial.com/wp-content/uploads/2017/04/png.jpg"
+                            style="max-width: 5em;">
+                    </a>
+                    <Button class="btn btn-primary" style="background-color: orangered; border-color: orangered"><a
+                            class="nav-link" href="{{ route('login') }}" style="color:white">{{ __('Iniciar sesión')
+                            }}</a></Button>
                     @endif
                 </div>
 
@@ -77,8 +114,9 @@
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
         <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js" crossorigin="anonymous">
-        </script>
+        <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"
+            crossorigin="anonymous">
+            </script>
 
         @yield('scripts')
 
